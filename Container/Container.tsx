d8632e29f10b18f5209ebccbb48a547d6b2b7e6a
dhkim <dhkim@mleverage.co.kr>
2025-04-15 14:25:28 +0900

    feat: Container 수정

--- Container/Container.tsx
+++ Container/Container.tsx
@@ -3,19 +3,21 @@ import styles from './Container.module.scss';
 
 type ContainerProps = {
     children: React.ReactNode;
-    gap?: number;
-    paddingY?: number;
+    maxWidth?: number;
+    gap?: 0 | 15 | 20 | 25 | 30 | 35 | 40 | 45 | 50 | 60 | 70 | 80 | 90 | 100 | 110 | 120 | 130 | 150;
+    paddingY?: 0 | 15 | 20 | 25 | 30 | 35 | 40 | 45 | 50 | 60 | 70 | 80 | 90 | 100 | 110 | 120 | 130 | 150;
 };
 
 interface CSSPropertiesWithVars extends React.CSSProperties {
     [key: `--${string}`]: string | number;
 }
 
-const Container = ({ gap = 0, paddingY = 0, children }: ContainerProps) => {
+const Container = ({ maxWidth = 1280, gap = 0, paddingY = 0, children }: ContainerProps) => {
     const computedGap = `var(--space-${gap})`;
     const computedPaddingY = `var(--space-${paddingY})`;
 
     const cssVariables: CSSPropertiesWithVars = {
+        '--max-width': `${maxWidth}px`,
         '--gap': computedGap,
         '--padding-y': computedPaddingY,
     };
